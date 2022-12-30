277a88b7c0a48b8f407c6a5faa864499552d2557
Zaixi <zxshang@utexas.edu>
2022-12-29 22:32:42 -0600

    support live hdr

--- calculate_hdrgreed_features.py
+++ calculate_hdrgreed_features.py
@@ -12,13 +12,24 @@ import socket
 parser = argparse.ArgumentParser()
 
 parser.add_argument(
-    "--nonlinear", help="select the nonliearity. Support 'local_exp', 'global_exp', 'equal' or 'none'.")
+    "--nonlinear", help="select the nonliearity. Support 'lnl'(for single exponential) ,'local_exp', 'global_exp', 'equal' or 'none'.")
 parser.add_argument(
     "--parameter", help="the parameter for the nonliear. Use with --nonliear", type=float)
+parser.add_argument(
+    "--band_pass", help="select the bandpass in NSS. Support 'SPyr', 'NS', 'MSCN', 'DoG'")
 parser.add_argument(
     "--wsize", help="the parameter for the nonliear window size. Use with --nonliear and local transform.", type=float)
 parser.add_argument(
     "--channel", help="indicate which channel to process. Please provide 0, 1, or 2", type=int)
+parser.add_argument(
+    "--dog_param1", help="Dog sigma low", type=int, default=60)
+parser.add_argument(
+    "--dog_param2", help="Dog sigma high", type=int, default=90)
+parser.add_argument(
+    "--v1lhe", help="LHE after nonlinear", action='store_true')
+parser.add_argument(
+    "--footprint", help="footprint of LHE transform", type=int, default=15)
+parser.set_defaults(v1lhe=False)
 
 
 if socket.gethostname().find('tacc') > 0:
@@ -43,6 +54,9 @@ elif socket.gethostname().find('stormtrooper') >= 0:  # stormtrooper
     out_root_vif = '/media/zaixi/zaixi_nas/HDRproject/feats/hdrvifnew/vif'
     out_root_dlm = '/media/zaixi/zaixi_nas/HDRproject/feats/hdrdlmnew/dlm'
 
+elif socket.gethostname().find('a51999') >= 0:  # Diaochan
+    vid_pth = '/mnt/31393986-51f4-4175-8683-85582af93b23/videos/HDR_2021_fall_yuv_upscaled/fall2021_hdr_upscaled_yuv'
+    out_root = '/media/zaixi/zaixi_nas/HDRproject/feats/fr_evaluate_HDRLIVE_correct/greed'
 
 args = parser.parse_args()
 # ref_path = '/media/josh-admin/nebula_josh/hdr/fall2021_hdr_upscaled_yuv/4k_ref_UrbanLandmark_upscaled.yuv'
@@ -62,13 +76,30 @@ def process_video(ind):
 
         feats = hdr_greed(join(vid_pth, video),
                           join(vid_pth, ref), fcount, args)
-        df = pd.DataFrame(feats).transpose()
-        df['video'] = bname
-        return df
+        feats.to_csv(os.path.join(outpth,bname.replace('.yuv','.csv')))
+        # df = pd.DataFrame(feats).transpose()
+        # df['video'] = bname
+        return feats
     return
 
+if args.band_pass.lower() != 'dog':
+    if args.v1lhe:
+        outpth = join(
+            out_root, f'greed_{args.nonlinear}_{args.parameter}_w{args.wsize}_c{args.channel}_band{args.band_pass}_v1lhe_{args.footprint}')
+    else:
+        outpth = join(
+            out_root, f'greed_{args.nonlinear}_{args.parameter}_w{args.wsize}_c{args.channel}_band{args.band_pass}')
 
-r = Parallel(n_jobs=1, verbose=1, backend="multiprocessing")(
+else:
+    if args.v1lhe:
+        outpth = join(
+            out_root, f'greed_{args.nonlinear}_{args.parameter}_w{args.wsize}_c{args.channel}_band{args.band_pass}-{args.dog_param1}-{args.dog_param2}_v1lhe_{args.footprint}')
+    else:
+        outpth = join(
+            out_root, f'greed_{args.nonlinear}_{args.parameter}_w{args.wsize}_c{args.channel}_band{args.band_pass}-{args.dog_param1}-{args.dog_param2}')
+os.makedirs(outpth,exist_ok=True)
+
+r = Parallel(n_jobs=80, verbose=1, backend="multiprocessing")(
     delayed(process_video)(i) for i in range(len(info)))
 feats = pd.concat(r)
 outpth = join(
--- combine.py
+++ combine.py
@@ -1,7 +1,7 @@
 import glob, os 
 import pandas as pd
 
-csvs = glob.glob('/home/zs5397/code/GREED/temp_feat/greed_none_5.0_w31.0_c0_bandDoG-60-90/*.csv')
+csvs = glob.glob('/home/zs5397/code/GREED/temp_feat/greed_lhe_None_w31.0_c0_bandmscn/*.csv')
 
 lists = []
 
@@ -9,4 +9,4 @@ for c in csvs:
     lists.append(pd.read_csv(c,index_col = 0))
 
 feats = pd.concat(lists)
-feats.to_csv('/home/zs5397/code/GREED/temp_feat/greed_none_5.0_w31.0_c0_bandDoG-60-90/feats.csv')
+feats.to_csv('/home/zs5397/code/GREED/temp_feat/greed_lhe_None_w31.0_c0_bandmscn/feats.csv')
